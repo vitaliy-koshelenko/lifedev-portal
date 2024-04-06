5617d78577fc449c229b76b5ce180d87e982bf8e
Vitaliy Koshelenko <vetal324@gmail.com>
2024-04-06 19:50:34 +0300

    [LifeDev Portal] MyProfile: Form - Storage Adapter Fix

--- modules/lifedev-myprofile/lifedev-myprofile-form/lifedev-myprofile-form-storage-adapter/src/main/java/com/lifedev/myprofile/form/storage/adapter/UserDDMStorageAdapter.java
+++ modules/lifedev-myprofile/lifedev-myprofile-form/lifedev-myprofile-form-storage-adapter/src/main/java/com/lifedev/myprofile/form/storage/adapter/UserDDMStorageAdapter.java
@@ -1,17 +1,32 @@
 package com.lifedev.myprofile.form.storage.adapter;
 
 import com.liferay.dynamic.data.mapping.exception.StorageException;
+import com.liferay.dynamic.data.mapping.form.values.factory.DDMFormValuesFactory;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceVersion;
+import com.liferay.dynamic.data.mapping.model.DDMStructureVersion;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceVersionLocalService;
 import com.liferay.dynamic.data.mapping.storage.*;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import javax.portlet.PortletSession;
 import java.util.List;
 import java.util.Locale;
 
@@ -46,12 +61,12 @@ public class UserDDMStorageAdapter implements DDMStorageAdapter {
     public DDMStorageAdapterSaveResponse save(DDMStorageAdapterSaveRequest ddmStorageAdapterSaveRequest) throws StorageException {
 
         try {
+            List<DDMFormFieldValue> ddmFormFieldValues = getDDMFormFieldValues(ddmStorageAdapterSaveRequest);
             DDMStorageAdapterSaveResponse defaultStorageAdapterSaveResponse = defaultStorageAdapter.save(ddmStorageAdapterSaveRequest);
 
             PermissionChecker permissionChecker = PermissionThreadLocal.getPermissionChecker();
             User currentUser = permissionChecker.getUser();
 
-            List<DDMFormFieldValue> ddmFormFieldValues = ddmStorageAdapterSaveRequest.getDDMFormValues().getDDMFormFieldValues();
             for (DDMFormFieldValue ddmFormFieldValue : ddmFormFieldValues) {
                 Locale locale = LocaleUtil.getDefault();
                 String fieldName = ddmFormFieldValue.getFieldReference();
@@ -72,10 +87,56 @@ public class UserDDMStorageAdapter implements DDMStorageAdapter {
         }
     }
 
+    private List<DDMFormFieldValue> getDDMFormFieldValues(DDMStorageAdapterSaveRequest ddmStorageAdapterSaveRequest) throws PortalException {
+        try {
+            return getDDMFormFieldValuesFromPortletRequest();
+        } catch (Exception e) {
+            _log.warn("Failed to get DDMFormFieldValue from request, cause: " + e.getMessage());
+            return getDDMFormFieldValuesFromStorageAdapterRequest(ddmStorageAdapterSaveRequest);
+        }
+    }
+
+    private List<DDMFormFieldValue> getDDMFormFieldValuesFromPortletRequest() throws PortalException {
+        List<DDMFormFieldValue> ddmFormFieldValues = null;
+        ServiceContext serviceContext = ServiceContextThreadLocal.getServiceContext();
+        LiferayPortletRequest portletRequest = serviceContext.getLiferayPortletRequest();
+        long formInstanceId = ParamUtil.getLong(portletRequest, "formInstanceId");
+        if (formInstanceId == 0) {
+            PortletSession portletSession = portletRequest.getPortletSession();
+            formInstanceId = GetterUtil.getLong(portletSession.getAttribute("DYNAMIC_DATA_MAPPING_FORM_INSTANCE_ID"));
+        }
+        DDMFormInstance ddmFormInstance = ddmFormInstanceLocalService.fetchDDMFormInstance(formInstanceId);
+        if (ddmFormInstance != null) {
+            DDMForm ddmForm = getDDMForm(ddmFormInstance);
+            DDMFormValues ddmFormValues = ddmFormValuesFactory.create(portletRequest, ddmForm);
+            ddmFormFieldValues = ddmFormValues.getDDMFormFieldValues();
+        }
+        return ddmFormFieldValues;
+    }
+
+    private List<DDMFormFieldValue> getDDMFormFieldValuesFromStorageAdapterRequest(DDMStorageAdapterSaveRequest ddmStorageAdapterSaveRequest) throws PortalException {
+        DDMFormValues ddmFormValues = ddmStorageAdapterSaveRequest.getDDMFormValues();
+        return ddmFormValues.getDDMFormFieldValues();
+    }
+
+    private DDMForm getDDMForm(DDMFormInstance ddmFormInstance) throws PortalException {
+        DDMFormInstanceVersion latestDDMFormInstanceVersion =
+                ddmFormInstanceVersionLocalService.getLatestFormInstanceVersion(ddmFormInstance.getFormInstanceId(),
+                        WorkflowConstants.STATUS_APPROVED);
+        DDMStructureVersion ddmStructureVersion = latestDDMFormInstanceVersion.getStructureVersion();
+        return ddmStructureVersion.getDDMForm();
+    }
+
     @Reference
     private UserLocalService userLocalService;
     @Reference(target = "(ddm.storage.adapter.type=default)")
     private DDMStorageAdapter defaultStorageAdapter;
+    @Reference
+    private DDMFormValuesFactory ddmFormValuesFactory;
+    @Reference
+    private DDMFormInstanceLocalService ddmFormInstanceLocalService;
+    @Reference
+    private DDMFormInstanceVersionLocalService ddmFormInstanceVersionLocalService;
 
     private static final Log _log = LogFactoryUtil.getLog(UserDDMStorageAdapter.class);
 }
