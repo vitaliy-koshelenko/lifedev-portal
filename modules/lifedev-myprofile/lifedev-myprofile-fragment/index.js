3ab206354aff79bf78f318f285743a3027132dd8
Vitaliy Koshelenko <vetal324@gmail.com>
2024-04-07 10:39:58 +0300

    [LifeDev Portal] MyProfile: Fragment - v2.1

--- modules/lifedev-myprofile/lifedev-myprofile-fragment/index.js
+++ modules/lifedev-myprofile/lifedev-myprofile-fragment/index.js
@@ -1,67 +1,43 @@
-document.getElementById("profileForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
+fragmentElement.querySelector("#myProfileForm").addEventListener("submit", function(event) {
+	event.preventDefault();
+	let form = this;
 
-    // Get form data
-    let form = this;
+	let firstName = form.elements["firstName"].value;
+	let lastName = form.elements["lastName"].value;
+	let email = form.elements["email"].value;
+	let userId = Liferay.ThemeDisplay.getUserId();
 
-    // Get form field values
-    let firstName = form.elements["firstName"].value;
-    let lastName = form.elements["lastName"].value;
-    let email = form.elements["email"].value;
+	let response = Liferay.Util.fetch(themeDisplay.getPortalURL() + "/o/headless-admin-user/v1.0/user-accounts/" + userId, {
+		method: 'PATCH',
+		headers: {
+			"Content-Type": "application/json",
+			'x-csrf-token': Liferay.authToken,
+		},
+		body: JSON.stringify({
+			"familyName": lastName,
+			"givenName": firstName,
+			"emailAddress": email
+		})
+	}).then(function(response) {
+		if (!response.ok) {
+			throw new Error("HTTP error, status = " + response.status);
+		}
+		return response.json();
+	}).then(function(response) {
+		console.log(response);
+		Liferay.Util.openToast({
+			message: 'User has been updated.',
+			type: 'success'
+		});
+	}).catch(function(error) {
+		console.error("Error:", error);
+		Liferay.Util.openToast({
+			message: 'Error occurred while updating user.',
+			type: 'danger'
+		});
+	});
+});
 
-    // Log form field values
-    console.log("firstName:", firstName);
-    console.log("lastName:", lastName);
-    console.log("email:", email);
-
-    console.log(JSON.stringify({
-        "familyName": lastName,
-        "givenName": firstName,
-        "email": email
-    }))
-
-    /*let response = Liferay.Util.fetch(themeDisplay.getPortalURL() + "/o/headless-admin-user/v1.0/user-accounts/" + formData.get("userId"), {
-        method: 'UPDATE',
-        headers: {
-            "Content-Type": "application/json",
-            'x-csrf-token': Liferay.authToken,
-        },
-        body: JSON.stringify({
-                givenName: "Ivan",
-                familyName: "Volkov"
-        })
-        }).then(function(response) {
-    return response.json();
-}).then(function(response) {
-    const {familyName, givenName} = response;
-    console.log(response);
-    console.log(givenName);
-    console.log(familyName);
-}).catch(function(error) {
-    console.error(error);
-})*/
-
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("PATCH", themeDisplay.getPortalURL() + "/o/headless-admin-user/v1.0/user-accounts/20122");
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("x-csrf-token", Liferay.authToken);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            console.log("Request successful");
-            console.log(xhr.responseText);
-        } else {
-            console.error("Request failed with status", xhr.status);
-        }
-    };
-    xhr.onerror = function () {
-        console.error("Request failed");
-    };
-    xhr.send(JSON.stringify({
-        "familyName": lastName,
-        "givenName": firstName,
-        "emailAddress": email
-    }));
-
+fragmentElement.querySelector("#cancel-button").addEventListener("click", function(event) {
+	window.location.reload();
 });
